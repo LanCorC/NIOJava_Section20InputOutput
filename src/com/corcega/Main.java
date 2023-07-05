b8fe2ceb12c2186126f206daebf2f6b46906100b
Lance <lanc3.corcega@gmail.com>
2023-07-05 17:20:38 +1200

    Section 20.331(JavaFX; Reading Files with NIO): introduction to passing index to buffer.getInt() etc as alternative instead of using buffer.flip(); demonstrated how to read data from a channel via reading data into buffers; using buffer.array() and .hasArray() as checks for possible ways to retrieve data from a buffer

--- src/com/corcega/Main.java
+++ src/com/corcega/Main.java
@@ -1,32 +1,16 @@
 package com.corcega;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
         try (FileOutputStream binFile = new FileOutputStream("data.dat");
         FileChannel  binChannel = binFile.getChannel()){
-//            FileInputStream file = new FileInputStream("data.txt");
-//            FileChannel channel = file.getChannel();
-
-//            Path dataPath = Paths.get("data.txt");
-//            Files.write(dataPath, "\nLine 5".getBytes(
-//                    StandardCharsets.UTF_8), StandardOpenOption.APPEND);
-//            List<String> lines = Files.readAllLines(dataPath);
-//            for(String line : lines) {
-//                System.out.println(line);
-//            }
-
             byte[] outputBytes = "Hello World!".getBytes();
             ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
             int numBytes = binChannel.write(buffer);
@@ -43,6 +27,51 @@ public class Main {
             intBuffer.flip();
             numBytes = binChannel.write(intBuffer);
             System.out.println("numBytes written was: " + numBytes);
+
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if (buffer.hasArray()) {
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+
+            //AbsoluteRead
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+
+//            //RelativeRead
+    //            intBuffer.flip();
+    //            numBytesRead = channel.read(intBuffer);
+    //            intBuffer.flip();
+    //            System.out.println(intBuffer.getInt());
+    //            intBuffer.flip();
+    //            numBytesRead = channel.read(intBuffer);
+    //            intBuffer.flip();
+    //            System.out.println(intBuffer.getInt());
+
+
+            channel.close();
+            ra.close();
+
+
+//            System.out.println("outputBytes = " + new String(outputBytes));
+
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            byte[] b = new byte[outputBytes.length];
+//            ra.read(b);
+//            System.out.println(new String(b));
+//
+//            long int1 = ra.readInt();
+//            long int2 = ra.readInt();
+//            System.out.println(int1);
+//            System.out.println(int2);
 
         } catch (IOException e) {
             e.printStackTrace();
