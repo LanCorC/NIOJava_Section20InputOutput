0b8feae2198206f4c5c154ae8d5876dda11c1c2c
LanCorC <lanc3.corcega@gmail.com>
2023-07-11 17:47:31 +1200

    Section 20.334(JavaFX; Writing Sequentially): part2, getting familiar with using positions in buffers and channels - random accessing a data source!

--- src/com/corcega/Main.java
+++ src/com/corcega/Main.java
@@ -44,22 +44,44 @@ public class Main {
 
             ByteBuffer readBuffer = ByteBuffer.allocate(100);
             channel.read(readBuffer);
-            readBuffer.flip();
-            byte[] inputString = new byte[outputBytes.length];
-            readBuffer.get(inputString);
-            System.out.println("inputString = " + new String(inputString));
-            System.out.println("int1 = " + readBuffer.getInt());
-            System.out.println("int2 = " + readBuffer.getInt());
-            byte[] inputString2 = new byte[outputBytes2.length];
-            readBuffer.get(inputString2);
-            System.out.println("inputString2 = " + new String(inputString2));
-            System.out.println("int3 = " + readBuffer.getInt());
 
             System.out.println("Now backwards: ");
             for (int i = 0; i < 3; i++) {
                 System.out.println(
                         readBuffer.getInt((int) numbersPositionInData[i]));
             }
+
+            byte[] outputString = "Hello, World!".getBytes();
+            long str1Pos = 0;
+            long newInt1Pos = outputString.length;
+            long newInt2Pos = newInt1Pos + Integer.BYTES;
+            byte[] outputString2 = "Nice to meet you".getBytes();
+            long str2Pos = newInt2Pos + Integer.BYTES;
+            long newInt3Pos = str2Pos + outputString2.length;
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            binChannel.position(newInt1Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            binChannel.position(int2Pos);
+            binChannel.write(intBuffer);
+
+            intBuffer.flip();
+            intBuffer.putInt(1000);
+            intBuffer.flip();
+            binChannel.position(int3Pos);
+            binChannel.write(intBuffer);
+
+            binChannel.position(str1Pos);
+            binChannel.write(ByteBuffer.wrap(outputString));
+            binChannel.position(str2Pos);
+            binChannel.write(ByteBuffer.wrap(outputString2));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
